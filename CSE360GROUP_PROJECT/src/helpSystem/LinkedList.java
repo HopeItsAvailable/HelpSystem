1c381eefd1d10f2a999fc0004b698218b0417958
cisco <cisco@macbook-m20.dhcp.asu.edu>
2024-10-09 12:12:53 -0700

    Updated the node and linkedList class. Added necessary variables besides username

--- CSE360GROUP_PROJECT/src/helpSystem/LinkedList.java
+++ CSE360GROUP_PROJECT/src/helpSystem/LinkedList.java
@@ -3,9 +3,18 @@ package helpSystem;
 public class LinkedList {
     protected Node head;
     private int size;
+//    private String username;
+//    private String password;
+//    private String email;
+//    private String firstName;
+//    private String middleName;
+//    private String lastName;
+//    private String preferredFirstName;
 
+    
     public LinkedList() {
-        this.head = null;
+    	this.head=null;
+    	this.size = 0;
     }
 
     // Method to add a node at the end of the list
@@ -22,11 +31,72 @@ public class LinkedList {
             while (current.next != null) {
                 current = current.next;
             }
-            
             current.next = newNode;
             size++;
         }
         
+    }
+    
+    public void finalizeUserEmail(String username, String email) {
+    	Node user = searchByUsername(username);
+    	if (user != null) {
+            user.setEmail(email);
+            System.out.println("Account for " + username + " finalized.");
+        } else {
+            System.out.println("User with username '" + username + "' not found.");
+        }
+    	
+    }
+    
+    public void finalizeUserFirstName(String username, String firstName) {
+        Node user = searchByUsername(username);
+        if (user != null) {
+            user.setFirstName(firstName);
+            System.out.println("First name for " + username + " updated.");
+        } else {
+            System.out.println("User with username '" + username + "' not found.");
+        }
+    }
+    
+    public void finalizeUserMiddleName(String username, String middleName) {
+        Node user = searchByUsername(username);
+        if (user != null) {
+            user.setMiddleName(middleName);
+            System.out.println("Middle name for " + username + " updated.");
+        } else {
+            System.out.println("User with username '" + username + "' not found.");
+        }
+    }
+
+    // Method to finalize the user's last name
+    public void finalizeUserLastName(String username, String lastName) {
+        Node user = searchByUsername(username);
+        if (user != null) {
+            user.setLastName(lastName);
+            System.out.println("Last name for " + username + " updated.");
+        } else {
+            System.out.println("User with username '" + username + "' not found.");
+        }
+    }
+
+    // Method to finalize the user's preferred first name
+    public void finalizeUserPreferredFirstName(String username, String preferredFirstName) {
+        Node user = searchByUsername(username);
+        if (user != null) {
+            user.setPreferredFirstName(preferredFirstName);
+            System.out.println("Preferred first name for " + username + " updated.");
+        } else {
+            System.out.println("User with username '" + username + "' not found.");
+        }
+    }
+    
+    public void addRoleToUser(String username, String role) {
+        Node user = searchByUsername(username);
+        if (user != null) {
+            user.addRole(role);  // Call the method in Node to add the role
+        } else {
+            System.out.println("User with username '" + username + "' not found.");
+        }
     }
 
     // Method to display the linked list
@@ -42,6 +112,7 @@ public class LinkedList {
             current = current.next;
         }
         System.out.println(size);
+        
     }
 
     // Method to search for a node by username
@@ -55,4 +126,7 @@ public class LinkedList {
         }
         return null;
     }
+    
+    
+    
 }
--- CSE360GROUP_PROJECT/src/helpSystem/Node.java
+++ CSE360GROUP_PROJECT/src/helpSystem/Node.java
@@ -1,18 +1,94 @@
 package helpSystem;
 
+import java.util.List;
+
 public class Node {
+	final private int TOTALNUMBEROFROLES = 3; //Number of roles. Currently admin, user, instructor.
     String username;
     String password;
+    private String email;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String preferredFirstName;
+    
     Node next;
+    
+    private String[] roles;  // Array to store roles
+    private int numOfRoles; 
 
+    //used on initial setup screen. Set methods are used to store other fields besides username/password
+    //when the user reaches account finalization screen.
     public Node(String username, String password) {
         this.username = username;
         this.password = password;
         this.next = null;
+        this.username = username;
+        this.password = password;
+        this.email = null;
+        this.firstName = null;
+        this.middleName = null;
+        this.lastName = null;
+        this.preferredFirstName = null;
+        this.numOfRoles = 1;
+        
+        this.roles = new String[TOTALNUMBEROFROLES];  // array with slots for each role
+        this.numOfRoles = 0;          
     }
 
-    @Override
+    public void addRole(String role) {
+    	if (numOfRoles == roles.length) {
+    		System.out.println("Max number of roles reached.");
+    		return;
+    	}
+    	
+    	// Check if the user already has that role 
+    	for (int i = 0; i < numOfRoles; i++) {
+    		if (roles[i].equals(role)) {
+    			System.out.println("User " + username + " already has the role " + role + ".");
+    			return;
+    		}
+    	}
+    	
+    	// Add the new role
+    	roles[numOfRoles] = role;
+    	numOfRoles++;
+    	System.out.println("Role " + role + " added to user " + username + ".");
+    }
+
+    // Setter methods to update additional fields later
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPreferredFirstName(String preferredFirstName) {
+        this.preferredFirstName = preferredFirstName;
+    }
+
+
+    //@Override
     public String toString() {
-        return "Username: " + username + ", Password: " + password;
+    	String displayRoles = "";
+        for (int i = 0; i < numOfRoles; i++) {
+        	displayRoles += (roles[i]);
+           
+        }
+        return "Username: " + username + ", Password: " + password + ", Email: " + email 
+                + ", Full Name: " + firstName + " " + middleName + " " + lastName 
+                + ", Preferred First Name: " + preferredFirstName
+                + ", Roles: " + displayRoles;
     }
 }
