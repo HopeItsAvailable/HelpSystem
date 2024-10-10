f694d0065af93770788a8f1dfaf226dfe9571197
cisco <cisco@macbook-m20.dhcp.asu.edu>
2024-10-09 20:41:17 -0700

    Now can assign multiple roles to a user from a OTP admin invite link.

--- CSE360GROUP_PROJECT/src/helpSystem/oneTimePasswordGenerator.java
+++ CSE360GROUP_PROJECT/src/helpSystem/oneTimePasswordGenerator.java
@@ -4,13 +4,13 @@ import java.util.*;
 
 public class oneTimePasswordGenerator { //linkedList for OTP's
     String code;
-    String role;
+    String[] roles; 
     long expirationTime; //expiration date and how long until it is reached
     oneTimePasswordGenerator next;
 
     public oneTimePasswordGenerator(String code, String[] roles, long expirationTime) {
         this.code = code;
-        this.role = role;
+        this.roles = roles;
         this.expirationTime = expirationTime;
         this.next = null;
     }
